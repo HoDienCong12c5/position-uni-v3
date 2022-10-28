804ec2243735be186728744070f4f0698537c012
HoDienCong12c5 <hodiecong12c5@gmail.com>
2022-10-28 15:12:46 +0700

    fire2

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
-import { ExampleComponent } from 'position-uni-v3'
-import 'position-uni-v3/dist/index.css'
+import {  usePool, usePosition,usePriceOrderingFromPositionForUI } from 'position-uni-v3'
+const ExamplePosition=()=>{
+  const [tokenPre, setTokenPre] = useState(null)
+  const [tokenSub, setTokenSub] = useState(null)
+  const [infoPositionBasic, setinfoPositionBasic] = useState(null)
+  // code ..
+  //
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const {poolHook,priceTokenPair} =usePool(tokenPre,tokenSub)
+  // code...
+  //
+  const {position}=usePosition(poolHook,infoPositionBasic)
+  const {priceLower,priceUpper,base,quote}=usePriceOrderingFromPositionForUI(position)
+  
+  useEffect(() => {
+    if (position && poolHook && tokenPre) {
+      const inverted = tokenPre ? base?.equals(tokenPre) : undefined
+
+      // const ratio = getRatio(
+      //   inverted ? priceLower : priceUpper.invert(),
+      //   poolHook.token0Price,
+      //   inverted ? priceUpper : priceLower.invert()
+      // )
+      //ratioRef.current = ratio
+      // ratioRef.current = 2
+    }
+  }, [position, poolHook, base, tokenPre])
 }
 
-export default App
+export default ExamplePosition
